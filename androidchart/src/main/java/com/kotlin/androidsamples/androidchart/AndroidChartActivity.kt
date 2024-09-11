0e2cdd96616cee2e54e0c99b7af57daaf118de32
Abdan Zaki Alifian <abdan.alifian@cimbniaga.co.id>
2024-09-11 22:41:30 +0700

    Add line chart 3

--- androidchart/src/main/java/com/kotlin/androidsamples/androidchart/AndroidChartActivity.kt
+++ androidchart/src/main/java/com/kotlin/androidsamples/androidchart/AndroidChartActivity.kt
@@ -25,6 +25,7 @@ class AndroidChartActivity : AppCompatActivity() {
         setContentView(binding.root)
         setupChart1()
         setupChart2()
+        setupChart3()
     }
 
     private fun setupChart1() {
@@ -101,6 +102,7 @@ class AndroidChartActivity : AppCompatActivity() {
 
     private fun setupChart2() {
         setDataChart2()
+
         binding.apply {
             // background color
             chart2.setBackgroundColor(Color.WHITE)
@@ -167,8 +169,78 @@ class AndroidChartActivity : AppCompatActivity() {
             val marker = CustomMarkerView(this@AndroidChartActivity)
             marker.chartView = chart2
             chart2.marker = marker
+        }
+    }
 
-            setDataChart1()
+    private fun setupChart3() {
+        setDataChart3()
+
+        binding.apply {
+            // background color
+            chart3.setBackgroundColor(Color.WHITE)
+
+            // no description text
+            chart3.description.isEnabled = false
+
+            // enable touch gestures
+            chart3.setTouchEnabled(true)
+
+            // enable scaling and dragging
+            chart3.setDragEnabled(true)
+            chart3.setScaleEnabled(false)
+            chart3.setDrawGridBackground(false)
+
+            chart3.animateX(600)
+
+            // get the legend (only possible after setting data)
+            chart3.legend.isEnabled = false
+
+            val xAxis = chart3.xAxis
+            xAxis.position = XAxis.XAxisPosition.BOTTOM
+            xAxis.enableGridDashedLine(10f, 10f, 10f)
+            xAxis.setDrawAxisLine(false)
+            xAxis.gridColor = ContextCompat.getColor(this@AndroidChartActivity, R.color.light_grey)
+            xAxis.textColor = ContextCompat.getColor(this@AndroidChartActivity, R.color.grey)
+            xAxis.textSize = 12F
+
+            val labels = ArrayList<Int>()
+            labels.add(200)
+            labels.add(400)
+            labels.add(600)
+            labels.add(800)
+            labels.add(1000)
+
+            val leftAxis = chart3.axisLeft
+            leftAxis.enableGridDashedLine(10F, 10F, 10F)
+            leftAxis.setDrawAxisLine(false)
+            leftAxis.axisMinimum = 0F
+            leftAxis.axisMaximum = labels.size.toFloat() - 1F
+            leftAxis.labelCount = labels.size - 1
+            leftAxis.valueFormatter = object : ValueFormatter() {
+                override fun getFormattedValue(value: Float): String {
+                    val index = value.toInt()
+                    if (index >= 0 && index < labels.size) {
+                        return buildString {
+                            append(labels[index])
+                            append("k")
+                        }
+                    }
+                    return ""
+                }
+            }
+            leftAxis.gridColor =
+                ContextCompat.getColor(this@AndroidChartActivity, R.color.light_grey)
+            leftAxis.textColor = ContextCompat.getColor(this@AndroidChartActivity, R.color.grey)
+            leftAxis.textSize = 12F
+
+            val rightAxis = chart3.axisRight
+            rightAxis.axisMinimum = labels.first().toFloat()
+            rightAxis.axisMaximum = labels.last().toFloat()
+            rightAxis.isEnabled = false
+
+            val marker = CustomMarkerView(this@AndroidChartActivity)
+            marker.chartView = chart3
+            chart3.marker = marker
         }
     }
 
@@ -316,6 +388,96 @@ class AndroidChartActivity : AppCompatActivity() {
 
                 // set data
                 chart2.data = data
+            }
+        }
+    }
+
+    private fun setDataChart3() {
+        val lowerBound = 200
+        val upperBound = 1000
+
+        val entryList: ArrayList<Entry> = ArrayList()
+        val entryList2: ArrayList<Entry> = ArrayList()
+        val calendars = ArrayList<Calendar>()
+
+        for (i in 7 downTo 0) {
+            val calendar = Calendar.getInstance()
+            calendar.add(Calendar.DAY_OF_YEAR, -i)
+            calendars.add(calendar)
+            val formatter = SimpleDateFormat("dd MMM yyyy", Locale.getDefault())
+            val date = formatter.format(calendar.time)
+            entryList.add(
+                Entry(
+                    7 - i.toFloat(),
+                    Random.nextInt(lowerBound, upperBound).toFloat(),
+                    date
+                )
+            )
+            entryList2.add(
+                Entry(
+                    7 - i.toFloat(),
+                    Random.nextInt(lowerBound, upperBound).toFloat(),
+                    date
+                )
+            )
+        }
+
+        binding.apply {
+            chart3.xAxis.axisMinimum = 0F
+            chart3.xAxis.axisMaximum = calendars.size.toFloat() - 1
+            chart3.xAxis.labelCount = calendars.size - 1
+            chart3.xAxis.setDrawLabels(false)
+//        chart.xAxis.valueFormatter = object : ValueFormatter() {
+//            override fun getFormattedValue(value: Float): String {
+//                val index = value.toInt()
+//
+//                if (index >= 0 && index < calendars.size) {
+//                    val futureDate = calendars[index].time
+//                    val formatter = SimpleDateFormat("dd/MM", Locale.getDefault())
+//                    val date = formatter.format(futureDate)
+//                    return date
+//                }
+//                return ""
+//            }
+//        }
+
+            val lineDataSet: LineDataSet?
+            val lineDataSet2: LineDataSet?
+
+            if (chart3.data != null && chart3.data.dataSetCount > 0) {
+                lineDataSet = chart3.data.getDataSetByIndex(0) as? LineDataSet
+                lineDataSet2 = chart3.data.getDataSetByIndex(1) as? LineDataSet
+                lineDataSet?.values = entryList
+                lineDataSet2?.values = entryList2
+                chart3.data.notifyDataChanged()
+                chart3.notifyDataSetChanged()
+            } else {
+                lineDataSet = LineDataSet(entryList, "")
+                lineDataSet.axisDependency = AxisDependency.RIGHT
+                lineDataSet.color = ContextCompat.getColor(this@AndroidChartActivity, R.color.green)
+                lineDataSet.lineWidth = 3f
+                lineDataSet.setDrawCircles(false)
+                lineDataSet.setDrawValues(false)
+                lineDataSet.setDrawFilled(false)
+                lineDataSet.mode = LineDataSet.Mode.HORIZONTAL_BEZIER
+
+                //set2.setFillFormatter(new MyFillFormatter(900f));
+                lineDataSet2 = LineDataSet(entryList2, "")
+                lineDataSet2.axisDependency = AxisDependency.RIGHT
+                lineDataSet2.color = ContextCompat.getColor(this@AndroidChartActivity, R.color.red)
+                lineDataSet2.lineWidth = 3f
+                lineDataSet2.setDrawCircles(false)
+                lineDataSet2.setDrawValues(false)
+                lineDataSet2.setDrawFilled(false)
+                lineDataSet2.mode = LineDataSet.Mode.HORIZONTAL_BEZIER
+
+                // create a data object with the data sets
+                val data = LineData(lineDataSet, lineDataSet2)
+                data.setValueTextColor(Color.BLACK)
+                data.setValueTextSize(9f)
+
+                // set data
+                chart3.data = data
             }
         }
     }
