3a5726d2169a73589a38942107a1ba9dc3048efa
Abdan Zaki Alifian <abdan.alifian@cimbniaga.co.id>
2024-09-17 14:30:25 +0700

    Add content to marker chart

--- androidchart/src/main/java/com/kotlin/androidsamples/androidchart/CustomMarkerViewWithoutPointer.kt
+++ androidchart/src/main/java/com/kotlin/androidsamples/androidchart/CustomMarkerViewWithoutPointer.kt
@@ -3,9 +3,15 @@ package com.kotlin.androidsamples.androidchart
 import android.content.Context
 import android.graphics.Canvas
 import android.view.LayoutInflater
+import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.MarkerView
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.utils.MPPointD
 import com.github.mikephil.charting.utils.MPPointF
 import com.kotlin.androidsamples.androidchart.databinding.CustomMarkerViewWithoutPointerBinding
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class CustomMarkerViewWithoutPointer(context: Context) :
     MarkerView(context, R.layout.custom_marker_view_without_pointer) {
@@ -19,10 +25,38 @@ class CustomMarkerViewWithoutPointer(context: Context) :
 
     override fun draw(canvas: Canvas, posX: Float, posY: Float) {
         super.draw(canvas, posX, posY)
+        val lineDataSetBuy = getLineDataSet(0)
+        val lineDataSetSell = getLineDataSet(1)
+
+        val pointBuy = getPoint(lineDataSetBuy, posX, posY)
+        val pointSell = getPoint(lineDataSetSell, posX, posY)
+
+        val entryBuy = getEntry(lineDataSetBuy, pointBuy)
+        val entrySell = getEntry(lineDataSetSell, pointSell)
+
         binding.tooltipView.setCoordinates(posX, chartView.width)
+        binding.tooltipView.setEntry(entryBuy, entrySell)
     }
 
     override fun getOffset(): MPPointF {
-        return MPPointF(-(binding.tooltipView.width  / 2).toFloat(), -binding.tooltipView.height.toFloat())
+        return MPPointF(-(width  / 2).toFloat(), -height.toFloat())
+    }
+
+    private fun getLineDataSet(index: Int): LineDataSet? {
+        return chartView.data.getDataSetByIndex(index) as? LineDataSet
+    }
+
+    private fun getPoint(lineDataSet: LineDataSet?, posX: Float, posY: Float): MPPointD? {
+        val lineChart = chartView as? LineChart
+        val axisDependency = lineDataSet?.axisDependency
+        return lineChart?.getTransformer(axisDependency)?.getValuesByTouchPoint(posX, posY)
+    }
+
+    private fun getEntry(lineDataSet: LineDataSet?, point: MPPointD?): Entry? {
+        return lineDataSet?.values?.find { entry ->
+            val entryX = BigDecimal(entry.x.toDouble()).setScale(1, RoundingMode.HALF_UP).toDouble()
+            val pointX = BigDecimal(point?.x ?: 0.0).setScale(1, RoundingMode.HALF_UP).toDouble()
+            entryX == pointX
+        }
     }
 }
--- androidchart/src/main/java/com/kotlin/androidsamples/androidchart/TooltipView.kt
+++ androidchart/src/main/java/com/kotlin/androidsamples/androidchart/TooltipView.kt
@@ -6,8 +6,11 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.graphics.RectF
+import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.ContextCompat
+import com.github.mikephil.charting.data.Entry
 
 class TooltipView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -17,9 +20,17 @@ class TooltipView @JvmOverloads constructor(
 
     private var chartWidth: Int? = null
 
+    private var entryBuy: Entry? = null
+
+    private var entrySell: Entry? = null
+
     private val rectF = RectF()
 
     private val path = Path()
+
+    private val buyText = "Harga beli/gram"
+
+    private val sellText = "Harga jual/gram"
 
     fun setCoordinates(highlightX: Float, chartWidth: Int) {
         this.highlightX = highlightX
@@ -31,26 +42,68 @@ class TooltipView @JvmOverloads constructor(
         }
     }
 
+    fun setEntry(entryBuy: Entry?, entrySell: Entry?) {
+        this.entryBuy = entryBuy
+        this.entrySell = entrySell
+    }
+
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         color = Color.WHITE
         style = Paint.Style.FILL
         setShadowLayer(5f, 0f, 5f, Color.GRAY)
     }
 
-    private val cornerRadius = 10f.dpToPx()
-    private val triangleHeight = 10f.dpToPx()
-    private val triangleWidth = 20f.dpToPx()
-    private val padding = 4f.dpToPx()
+    private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.BLACK
+        textSize = 9f.dpToPx
+        textAlign = Paint.Align.LEFT
+    }
 
-    private fun Float.dpToPx(): Float = this * resources.displayMetrics.density
+    private val valuePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        textSize = 9f.dpToPx
+        textAlign = Paint.Align.RIGHT
+        typeface = Typeface.create(Typeface.DEFAULT, Typeface.BOLD)
+    }
+
+    private val datePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.GRAY
+        textSize = 6f.dpToPx
+        textAlign = Paint.Align.CENTER
+    }
+
+    private val buyPaint =
+        Paint(valuePaint).apply { color = ContextCompat.getColor(context, R.color.green) }
+
+    private val sellPaint =
+        Paint(valuePaint).apply { color = ContextCompat.getColor(context, R.color.red) }
+
+    private val cornerRadius = 10f.dpToPx
+    private val triangleHeight = 10f.dpToPx
+    private val triangleWidth = 20f.dpToPx
+    private val padding = 4f.dpToPx
+
+    private val Float.dpToPx: Float
+        get() = this * resources.displayMetrics.density
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
         highlightX?.let { xCoordinate ->
-            val rectLeft = padding
+            val buyPriceTextWidth = textPaint.measureText(buyText)
+            val buyPriceValueWidth = buyPaint.measureText(entryBuy?.data.toString())
+            val sellPriceTextWidth = textPaint.measureText(sellText)
+            val sellPriceValueWidth = sellPaint.measureText(entrySell?.data.toString())
+
+            val maxTextWidth = maxOf(
+                buyPriceTextWidth + buyPriceValueWidth,
+                sellPriceTextWidth + sellPriceValueWidth
+            )
+
+            val rectWidth = maxTextWidth + 2 * 20f.dpToPx
+
+            val rectLeft = (width - rectWidth) / 2f
             val rectTop = padding
-            val rectRight = width - padding
+            val rectRight = rectLeft + rectWidth
             val rectBottom = height - triangleHeight - padding
 
             rectF.set(rectLeft, rectTop, rectRight, rectBottom)
@@ -59,7 +112,7 @@ class TooltipView @JvmOverloads constructor(
             path.reset()
 
             val triangleCenterX = when {
-                xCoordinate > width -> width - ((chartWidth ?: 0) - xCoordinate)
+                xCoordinate / 2 > width -> width - ((chartWidth ?: 0) - xCoordinate)
                 xCoordinate > width / 2f -> width / 2f
                 else -> xCoordinate
             }
@@ -72,7 +125,79 @@ class TooltipView @JvmOverloads constructor(
             path.close()
 
             canvas.drawPath(path, paint)
+
+            entryBuy?.data?.let { date ->
+                canvas.drawText(
+                    date.toString(),
+                    width / 2f,
+                    rectTop + 12f.dpToPx,
+                    datePaint
+                )
+            }
+
+            entryBuy?.y?.let { buyPrice ->
+                canvas.drawText(
+                    buyText,
+                    rectLeft + 8f.dpToPx,
+                    rectTop + 8f.dpToPx + 18f.dpToPx,
+                    textPaint
+                )
+
+                canvas.drawText(
+                    buildString {
+                        append("IDR")
+                        append(" ")
+                        append(buyPrice.toInt())
+                        append(",000.00")
+                    },
+                    rectRight - 8f.dpToPx,
+                    rectTop + 8f.dpToPx + 18f.dpToPx,
+                    buyPaint
+                )
+            }
+
+            entrySell?.y?.let { sellPrice ->
+                canvas.drawText(
+                    sellText,
+                    rectLeft + 8f.dpToPx,
+                    rectTop + 8f.dpToPx + 2 * 16f.dpToPx,
+                    textPaint
+                )
+
+                canvas.drawText(
+                    buildString {
+                        append("IDR")
+                        append(" ")
+                        append(sellPrice.toInt())
+                        append(",000.00")
+                    },
+                    rectRight - 8f.dpToPx,
+                    rectTop + 8f.dpToPx + 2 * 16f.dpToPx,
+                    sellPaint
+                )
+            }
         }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val buyPriceTextWidth = textPaint.measureText(buyText)
+        val buyPriceValueWidth = buyPaint.measureText(entryBuy?.data.toString())
+        val sellPriceTextWidth = textPaint.measureText(sellText)
+        val sellPriceValueWidth = sellPaint.measureText(entrySell?.data.toString())
+
+        val maxTextWidth = maxOf(
+            buyPriceTextWidth + buyPriceValueWidth,
+            sellPriceTextWidth + sellPriceValueWidth
+        )
+
+        val desiredWidth = maxTextWidth + 4 * 20f.dpToPx
+
+        val desiredHeight = 2 * 18f.dpToPx + 2 * 12f.dpToPx + triangleHeight
+
+        val width = resolveSize(desiredWidth.toInt(), widthMeasureSpec)
+        val height = resolveSize(desiredHeight.toInt(), heightMeasureSpec)
+
+        setMeasuredDimension(width, height)
     }
 }
 
