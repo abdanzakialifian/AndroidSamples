e567338ef137366c3df0a7426a1efc54b721c267
Abdan Zaki Alifian <abdanzakialifian99@gmail.com>
2026-02-17 22:25:46 +0700

    chore : added custom ParcelableNavType.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     alias(libs.plugins.kotlin.ksp)
     alias(libs.plugins.dagger.hilt.android)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlin.parcelize)
 }
 
 android {
