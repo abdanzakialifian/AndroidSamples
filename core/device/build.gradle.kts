b62a2bfbdf720c7d22e134bf15ba87d433b614ff
Abdan Zaki Alifian <abdanzakialifian99@gmail.com>
2026-03-04 20:17:18 +0700

    chore : add new device module

---
+++ core/device/build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.android.playground.device"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+}
+
+dependencies {
+    api(libs.play.services.wearable)
+    api(libs.kotlinx.coroutines.play.services)
+}
