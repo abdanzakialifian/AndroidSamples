232493f79835fc9ccc50d9c88be3d329651d8a94
Abdan Zaki Alifian <abdanzakialifian99@gmail.com>
2026-04-05 15:41:06 +0700

    chore : handling safe area windows

--- app/src/main/java/com/android/playground/MyApplication.kt
+++ app/src/main/java/com/android/playground/MyApplication.kt
@@ -1,7 +1,12 @@
 package com.android.playground
 
+import android.app.Activity
 import android.app.Application
 import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.android.playground.di.mainModule
 import com.google.android.play.core.splitcompat.SplitCompat
 import org.koin.android.ext.koin.androidContext
@@ -10,6 +15,7 @@ import org.koin.core.context.startKoin
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        activityLifecycleCallbacks()
         startKoin {
             androidContext(applicationContext)
             modules(mainModule)
@@ -19,5 +25,38 @@ class MyApplication : Application() {
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
         SplitCompat.install(this)
+    }
+
+    private fun activityLifecycleCallbacks() {
+        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks{
+            override fun onActivityCreated(activity: Activity, p1: Bundle?) {
+                val rootView = activity.window.decorView.findViewById<View>(android.R.id.content)
+                ViewCompat.setOnApplyWindowInsetsListener(rootView) { view, insets ->
+                    val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                    view.setPadding(
+                        systemBars.left,
+                        systemBars.top,
+                        systemBars.right,
+                        systemBars.bottom
+                    )
+                    insets
+                }
+            }
+
+            override fun onActivityStarted(p0: Activity) {}
+
+            override fun onActivityResumed(p0: Activity) {}
+
+            override fun onActivityPaused(p0: Activity) {}
+
+            override fun onActivityStopped(p0: Activity) {}
+
+            override fun onActivitySaveInstanceState(
+                p0: Activity,
+                p1: Bundle
+            ) {}
+
+            override fun onActivityDestroyed(p0: Activity) {}
+        })
     }
 }
