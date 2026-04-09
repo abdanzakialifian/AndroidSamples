21edbcfbbddaea76c89de88f2fa38ee53b61d975
Abdan Zaki Alifian <abdanzakialifian99@gmail.com>
2026-04-09 08:14:16 +0700

    chore : added sonarqube dependencies and adjust CI workflow for instrumentation test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     alias(libs.plugins.kotlin.ksp)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.sonarqube)
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,5 @@ plugins {
     alias(libs.plugins.kotlin.ksp) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.sonarqube) apply false
 }
